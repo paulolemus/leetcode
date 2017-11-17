ca41aa070dccc19029b4aa9d1cd21171bc7bda19
Paulo Lemus <paulolemus14@gmail.com>
2017-11-16 17:59:22 -1000

    Added 1 Python solution

---
+++ Python/add_digits.py
@@ -0,0 +1,28 @@
+# Source: https://leetcode.com/problems/add-digits/description/
+# Author: Paulo Lemus
+# Date  : 2017-11-16
+# Info  : #258, Easy
+
+# Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+#
+# For example:
+#
+# Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+#
+# Follow up:
+# Could you do it without any loop/recursion in O(1) runtime?
+#
+# Credits:
+# Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+
+
+class Solution:
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while num > 9:
+            num = sum(map(int, str(num)))
+        return num
+
